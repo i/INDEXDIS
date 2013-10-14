322e1ffedec35b2c10a04f72fee3f1d56210d08f
Ian Lozinski <ian.lozinski@gmail.com>
2013-10-14 23:23:35 +0000

    Reading from single file

--- main.c
+++ main.c
@@ -10,9 +10,9 @@
 int main(int argc, char **argv) {
 /*  struct dirent *ep;*/
 /*  DIR *dp;*/
-/*  FILE *fp;*/
+  FILE *fp;
 /*  char *path = malloc(128);*/
-/*  char *buff, *s;*/
+  char *buff;
   node trie = createTree();
 /*  TokenizerT *tok;*/
 
@@ -21,41 +21,20 @@ int main(int argc, char **argv) {
 /*  }*/
 
   /* Try opening as a file first. */
-/*  if ((fp = fopen(argv[1], "r"))) {*/
-/*    buff = malloc(256);*/
-/*    while(fscanf(fp, "%s\n", buff) != EOF) {*/
-/*      tok = TKCreate(" ", buff);*/
-/*      while ((s = TKGetNextToken(tok))) {*/
-/*        addToTree(s, trie);*/
-/*      }*/
-/*    }*/
-/*    fclose(fp);*/
-/*  }*/
-
-  /* If it wasn't a file, hopefully it's a directory. */
-/*  else if ((dp = opendir(argv[1]))) {*/
-/**/
-/*  }*/
-/**/
-  /* If it was neither, we can't open it. */
-/*  else {*/
-/*    perror ("Couldn't open the directory");*/
-/*    free(path);*/
-/*    return 1;*/
-/*  }*/
-
-  char * buff;
-  char foo[] = "FOO";
-  char testicle[] = "balls";
-
-  addToTree(foo, trie);
-  addToTree(testicle, trie);
+  if ((fp = fopen(argv[1], "r"))) {
+    buff = calloc(1, 256);
+    while(fscanf(fp, "%s\n", buff) != EOF) {
+      addToTree(buff, trie);
+    }
+    free(buff);
+    fclose(fp);
+  }
 
 
-  buff = malloc(trie->height);
+  buff = calloc(1, trie->height);
+
   printTree(trie, buff);
   destroyTree(trie);
-
   free(buff);
   return 0;
 }
